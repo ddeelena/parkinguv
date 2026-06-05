c91ccf30301ab0948b499da3f87b427a834552f1
Derly <dquejada1028@cue.edu.co>
2026-06-05 14:12:50 -0500

    Refactor TDD

--- src/parking/parcking.py
+++ src/parking/parcking.py
@@ -5,18 +5,28 @@ PRECIO_POR_HORA = 500
 TOPE_DIARIO = 12000
 DESCUENTO_VIP = 0.20
 
-def calcular_cobro(minutos_estacionado, es_vip=False):
+def calcular_cobro_base(minutos_estacionado):
     if minutos_estacionado <= MINUTOS_GRATIS:
         return 0
-    
-    horas_completas = math.ceil((minutos_estacionado - MINUTOS_GRATIS) / 60)
-    cobro = horas_completas * PRECIO_POR_HORA
 
-    if cobro > TOPE_DIARIO:
-        cobro = TOPE_DIARIO
+    horas = math.ceil(
+        (minutos_estacionado - MINUTOS_GRATIS) / 60
+    )
+
+    return min(
+        horas * PRECIO_POR_HORA,
+        TOPE_DIARIO
+    )
+
+
+def calcular_cobro(minutos_estacionado, es_vip=False):
+    cobro = calcular_cobro_base(minutos_estacionado)
 
     if es_vip:
-        cobro *= (1 - DESCUENTO_VIP)
+        cobro = calcular_cobro_vip(cobro)
 
     return int(cobro)
+
+def calcular_cobro_vip(cobro):
+    return int(cobro * (1 - DESCUENTO_VIP))
 
